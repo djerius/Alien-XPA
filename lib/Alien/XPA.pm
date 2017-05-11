0272d91ad8c217de3d5428523af5dec501e559a4
Diab Jerius <djerius@cfa.harvard.edu>
2017-05-11 11:41:44 -0400

    initial import

---
+++ lib/Alien/XPA.pm
@@ -0,0 +1,23 @@
+package Alien::XPA;
+
+# ABSTRACT: a really awesome library
+
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+use base qw( Alien::Base );
+
+1;
+
+# COPYRIGHT
+
+__END__
+
+
+=head1 SYNOPSIS
+
+
+=head1 SEE ALSO
+
